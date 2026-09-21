aeb010b546a589255a184d2875f7848aadf46e5b
Shivam Singh <roxshivamsingh@gmail.com>
2026-09-21 20:55:28 +0530

    FIXED: Minor;

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:vanzara/database/note_database.dart';
 
-class NotePage extends StatelessWidget {
+class NotePage extends StatefulWidget {
   const new({super.key});
 
   @override
+  State<NotePage> createState() => _NotePageState();
+}
+
+class _NotePageState extends State<NotePage> {
+  List<Map<String, dynamic>> notes = [];
+
+  Future<void> fetchNotes() async {
+    final fetchNotes = await getNotes();
+    setState(() {
+      notes = fetchNotes;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [ElevatedButton(onPressed: () {}, child: Text("Text"))],
-      ),
-    );
+    return notes.isEmpty
+        ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.notes, size: 80, color: Colors.grey[600]),
+                SizedBox(height: 20),
+                Text(
+                  'No notes found.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[6],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Column(children: []);
   }
 }
